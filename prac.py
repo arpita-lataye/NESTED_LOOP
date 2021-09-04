92eff98c659c6a7d793901ea8ec73899f0ea4e11
arpita-lataye <89316522+arpita-lataye@users.noreply.github.com>
2021-09-04 11:40:45 +0530

    Add files via upload

---
+++ prac.py
@@ -0,0 +1,100 @@
+# sum of product if a number
+# i=int(input("enter the number:"))
+# pro=0
+# while i>0:
+#     pro=pro+(i%10)
+#     i=i//10
+# print("sum of product:",pro)          
+
+# sum of cube of a digit
+# i=int(input("enter the number:"))
+# pro=0
+# while i>0:
+#     pro=pro+(i%10)*(i%10)*(i%10)
+#     i=i//10
+#     print(pro)
+
+
+
+# check armstrong number
+# i=int(input("enter the number:"))
+# org=i
+# sum=0
+# while i>0:
+#     sum=sum+(i%10)*(i%10)*(i%10)
+#     i=i//10
+# if org==sum:
+#     print("armstrong")
+# else:
+#     print ("no")
+
+
+# product of a digit
+# i=int(input("enter the number:"))
+# pro=1
+# while i>0:
+#     pro=pro*(i%10)
+#     i=i//10
+#     print(pro)
+
+# polidrome 
+# i=int(input("enter the number:"))
+# rev=0
+# org=i
+# while i>0:
+#     rev=(rev*10)+(i%10)
+#     i=i//10
+# if org==rev:
+#     print("polidrome")
+# else:
+#     print("no polidrome")
+
+
+# i=1
+# while i<=5:
+#     b=1
+#     while b<=5-i:
+#         print(" ",end="")
+#         b=b+1
+#     j=1
+#     while j<=i:
+#         print("*",end="")
+#         j=j+1
+#     print()
+#     i=i+1
+
+
+# i=1
+# k=1
+# while i<=5:
+#     b=1
+#     while b<=5-i:
+#         print(" ",end='')
+#         b=b+1
+#     j=1
+#     while j<=k:
+#         print("*",end='')
+#         j=j+1
+#     k=k+2
+#     print()
+#     i=i+1
+
+# n=int(input("enter the number:"))
+# i=1
+# while (n>0):
+#     b=1
+#     while b<i:
+#         print(" ",end='')
+#         b=b+1
+#     j=1
+#     while (j<=(n*2)-1):
+#             print("*",end='')
+#             j=j+1
+#     print()
+#     n=n-1
+    # i=i+1
+
+
+
+
+
---
+++ task.py
@@ -0,0 +1,10 @@
+# que 7
+# sum of first 10 even numbers
+
+i=2
+sum=0
+while i<10:
+    if i%2==0:
+        sum=sum+i
+        i=i+2
+        print(sum)
